fa8e325c3738c662dbafcf7ab771db9a0a4a97f3
Justin Gaskins <gaskij@rpi.edu>
2019-05-27 14:02:09 -0400

    Added ability to show location on map

--- public/controllers/mapController.js
+++ public/controllers/mapController.js
@@ -1,49 +1,37 @@
 app.controller('mapController', function($scope, $http) {
-  //this function is called to pull up popup info for each page
-  // $scope.showOnMap = function(building, latitude, longitude) {
-  //     $.ajax({
-  //        url: "public/infoPreview.php",
-  //        type: "get",
-  //        datatype: "json",
-  //        data: {
-  //           "location": building
-  //        },
-  //        success: function(response){
-  //           var info = JSON.parse(response);
-  //           //load the data into html to go into the popup based on the mysql query in infopreview.php
-  //           var popupContent = '<form method="post" action="info.php"><div class="popup" onclick="javascript:this.parentNode.submit();"><h2>';
-  //           popupContent += info['location'] + '</h2>';
-  //           popupContent += '<p>Nicknames: ' + info['nicks'] + '</p>';
-  //           popupContent += '<img src="' + info['image'] + '" alt="' + info['location'] + '" width="100%"/><input type="text" name="location" value="';
-  //           popupContent += info['location'];
-  //           popupContent += '" style="display:none"><input type="text" name="type" value="none" style="display:none"></div></form>';
-  //
-  //           mymap.setView([latitude, longitude], 19);
-  //           L.marker([latitude, longitude]).addTo(mymap)
-  //               .bindPopup(popupContent).openPopup();
-  //        },
-  //        error: function(xhr){
-  //           alert(xhr);
-  //        }
-  //     });
-  // }
-
-
-
   console.log("mapController activated!");
 
   const location = getParams();
   console.log(location != null);
 
   if (location != null) {
-    $http.post(`/info?loc=${location.loc}`, {query: location.loc})
+    $http.post(`/index?loc=${location.loc}`, {loc: location.loc})
     .then(function(httpResponse, err) {
       if (err) throw err;
       console.log(httpResponse.data);
 
       $scope.name = httpResponse.data[0].properties.name;
-      $scope.nick = 'Nicknames: ' + httpResponse.data[0].properties.nick;
+      $scope.nick = `Nicknames: ${httpResponse.data[0].properties.nick}`;
       $scope.desc = httpResponse.data[0].properties.description;
+
+      let latitude = httpResponse.data[0].geometry.coordinates[1];
+      let longitude = httpResponse.data[0].geometry.coordinates[0];
+
+      let popupContent = `\
+        <a href="/info?loc=${httpResponse.data[0].id}"> \
+          <div class="popup"> \
+            <h5>${httpResponse.data[0].properties.name}</h5> \
+            <img src="${httpResponse.data[0].properties.thumbnail}" alt="${httpResponse.data[0].properties.name}" width="100%"/> \
+            <p>Nicknames: ${httpResponse.data[0].properties.nick}</p> \
+          </div> \
+        </a> \
+      `;
+
+      mymap.setView([latitude, longitude], 19);
+      let marker = L.marker([latitude, longitude]);
+      marker.addTo(mymap).bindPopup(popupContent).openPopup();
+      // mymap.on('click', marker.remove());
+
     });
   }
 
