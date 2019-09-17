5e2ebbbb2e6a23f86d5af89250fa7b9663b3c853
Gabriel Wild <45672441+gwild37@users.noreply.github.com>
2019-09-17 16:35:14 -0400

    Revert "Pwa"

--- public/views/pwabuilder-sw-register.js
+++ public/views/pwabuilder-sw-register.js
@@ -9,7 +9,7 @@ if ("serviceWorker" in navigator) {
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("sw.js", {
+      .register("pwabuilder-sw.js", {
         scope: "./"
       })
       .then(function (reg) {
