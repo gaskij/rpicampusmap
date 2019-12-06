b0853d144fa32a250361dc849b70a706ea32f017
Unachieved <jonpierreantoine@gmail.com>
2019-12-06 12:31:29 -0500

    slight changes

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 //  UserModel
 const User = require('../models/User');
@@ -97,11 +99,94 @@ router.post('/register', (req, res) => {
     }
 });
 
-
+router.get('/search',  (req, res) => res.render('searchResults', {page_name: "Search", layout: "layout2.ejs", extractStyles: true}));
+router.post('/search', jsonParser, function(req, res) {
+    const query = req.body.query;
+    console.log("Query:", query);
+  
+    MongoClient.connect(uri, options, function(err, db) {
+      if (err)
+        throw err;
+      else {
+        console.log("Database connected in route '/search'!")
+        let db1 = db.db("rpicampusmap");
+        let db2 = db.db("forgemill");
+        let results = [];
+  
+        // Search the database for locations matching the given regular expression
+        // Search by name and by nickname for any match of the substring
+        db1.collection("locations").find({'$or': [
+          {'properties.name': {'$regex': query, '$options': 'i'} },
+          {'properties.nick': {'$regex': query, '$options': 'i'} }
+          // add here to look through machines
+        ]}).toArray()
+        .then(function(result1) {
+          console.log("Result1:\n", result1);
+          results = results.concat(result1);
+        })
+        .catch(function(err) {
+          if (err)
+            console.error("ERROR:", err);
+        });
+  
+        db2.collection("locations").find({'$or': [
+          {'properties.name': {'$regex': query, '$options': 'i'} },
+          {'properties.nick': {'$regex': query, '$options': 'i'} },
+          {'contents.machines': {'$regex': query, '$options': 'i'} }
+        ]}).toArray()
+        .then(function(result2) {
+          console.log("Result2:\n", result2);
+          results = results.concat(result2);
+          console.log("Results:\n", results);
+          res.send(results);
+        })
+        .catch(function(err) {
+          if (err)
+            console.error("ERROR:", err);
+        });
+  
+        db.close();
+      }
+    });
+  
+  });
 
 // main page --- the map
 router.get('/main_page', (req, res) => res.render('map', {layout: "layout2.ejs", extractStyles: true}));
-
+router.post('/main_page', jsonParser, function(req, res) {
+    //get the location to highlight
+    const query = req.body.query;
+    const machine = req.body.machine;
+    console.log(req.body);
+    console.log("Query:", query);
+    console.log("Machine:", machine);
+  
+    MongoClient.connect(uri, options, function(err, db) {
+      if (err)
+        throw err;
+      else {
+        console.log("Database connected in route '/index'!")
+  
+        let dbo = db.db("rpicampusmap");
+  
+        // switch database if necessary
+        if (machine == "true")
+          dbo = db.db("forgemill");
+  
+        dbo.collection("locations").find({'id': query}).toArray()
+        .then(function(result) {
+          console.log("Results:\n", result);
+          res.send(result);
+        })
+        .catch(function(err) {
+          if (err)
+            console.error("ERROR:", err);
+        });
+        db.close();
+      }
+    });
+  
+  });
 
 
 module.exports = router; 
