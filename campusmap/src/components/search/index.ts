dd3c03cb27faacc7609a493e0f42272beae1527c
Justin Gaskins <gaskij@rpi.edu>
2020-05-03 04:44:50 -0400

    Remove AngularJS code and reserve campusmap/src/ for React code

--- campusmap/src/components/search/index.ts
+++
@@ -1,3 +0,0 @@
-import module from './search.module';
-
-export default module.name;
--- campusmap/src/components/search/search.module.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-import ngRoute from 'angular-route';
-
-const searchModule = angular.module('searchResults', [ngRoute]);
-
-export default searchModule;
