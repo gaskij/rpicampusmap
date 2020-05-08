d7dce5bb9aef362eedb343f447e02e917974c0a7
Gabrielle Ford <gabrielleford@Gabrielles-MBP.fios-router.home>
2020-05-08 10:01:43 -0400

    added not found page in react

--- campusmap/src/app/App.tsx
+++ campusmap/src/app/App.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import SearchResultsPage from 'campusmap/src/search-results';
+import NotFoundPage from 'campusmap/src/not-found'
 import Header from './Header';
 import Footer from './Footer';
 
@@ -15,6 +16,9 @@ const App = (): ReactElement => {
           <Route path="/search">
             <SearchResultsPage />
           </Route>
+          <Route path="/*">
+            <NotFoundPage />
+          </Route> 
         </Switch>
       </main>
       <Footer />
