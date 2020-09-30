8103a8855b6cfc30ca407d20360af5d70c18fac4
Justin Gaskins <gaskij@rpi.edu>
2020-09-30 02:54:53 -0400

    Point MongoDB connection to correct database

--- api/src/config/dbConfig.ts
+++ api/src/config/dbConfig.ts
@@ -6,6 +6,6 @@ dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 const USER = process.env.DB_USER;
 const PASS = process.env.DB_PASS;
 
-export const DB_URI = process.env.MONGODB_URI || `mongodb://${USER}:${PASS}@rpicampusmap-shard-00-00-fwvzb.gcp.mongodb.net:27017,rpicampusmap-shard-00-01-fwvzb.gcp.mongodb.net:27017,rpicampusmap-shard-00-02-fwvzb.gcp.mongodb.net:27017/test?ssl=true&replicaSet=rpicampusmap-shard-0&authSource=admin&retryWrites=true&w=majority`;
+export const DB_URI = process.env.MONGODB_URI || `mongodb://${USER}:${PASS}@rpicampusmap-shard-00-00-fwvzb.gcp.mongodb.net:27017,rpicampusmap-shard-00-01-fwvzb.gcp.mongodb.net:27017,rpicampusmap-shard-00-02-fwvzb.gcp.mongodb.net:27017/rpicampusmap?ssl=true&replicaSet=rpicampusmap-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 export const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
