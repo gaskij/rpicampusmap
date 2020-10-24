32d3ea033d05a5bbc31c91fbb90fce6b91275ba8
Justin Gaskins <gaskij@rpi.edu>
2020-10-23 21:48:17 -0400

    Rework PhotoCarousel to map array of images

--- campusmap/src/info/PhotoCarousel/PhotoCarousel.tsx
+++ campusmap/src/info/PhotoCarousel/PhotoCarousel.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { ReactElement, useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-import { Location } from 'campusmap/src/types';
-
+import { Location, Photo } from 'campusmap/src/types';
 
 interface Props {
   location: Location;
@@ -14,33 +13,43 @@ interface Props {
 const PhotoCarousel = ({ location }: Props): ReactElement => {
   const [index, setIndex] = useState(0);
 
+  const images: Photo[] = [
+    {
+      author: 'author 1',
+      description: 'alt text 1',
+      src: 'http://tours.pocketsights.com/media/1/11ef2a38f7884fd58ecc6a78e3e70ea7_large.jpg',
+      date: new Date(),
+    },
+    {
+      author: 'author 2',
+      description: 'alt text 2',
+      src: 'http://www.alpha6294.com/History/ThetaXionCampus/DarrinCommunicationsCenter/Building/DarrinCommunicationsCenter.jpg',
+      date: new Date(),
+    },
+    {
+      author: 'author 3',
+      description: 'alt text 3',
+      src: 'https://www.usnews.com/img/college-photo_3861.jpg',
+      date: new Date(),
+    },
+  ];
+
   const handleSelect = (selectedIndex: number, _e: object | null): void => {
     setIndex(selectedIndex);
   };
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="http://tours.pocketsights.com/media/1/11ef2a38f7884fd58ecc6a78e3e70ea7_large.jpg"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="http://www.alpha6294.com/History/ThetaXionCampus/DarrinCommunicationsCenter/Building/DarrinCommunicationsCenter.jpg"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.bing.com/th?id=OIP.npiBC_gFZvex2KDN16YTZAHaFj&pid=Api&rs=1&p=0"
-          alt="Third slide"
-        />
-      </Carousel.Item>
+      {images.map((image) => (
+        <Carousel.Item key={image.src}>
+          <img
+            className="d-block w-100"
+            style={{maxHeight: '300px', minHeight: 'auto'}}
+            src={image.src}
+            alt={image.description}
+          />
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 };
