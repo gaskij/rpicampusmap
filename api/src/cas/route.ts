ce6797eac91a431f51ebbf7b5aa0837fdc74a203
saprap1 <priyasapra.2017@gmail.com>
2021-03-23 18:02:34 -0400

    move logout to controller

--- api/src/cas/route.ts
+++ api/src/cas/route.ts
@@ -1,7 +1,6 @@
 /** Node Imports */
 import express, { Response } from 'express';
 import bodyParser from 'body-parser';
-import CASAuthentication from 'express-cas-authentication';
 
 /** Custom Imports */
 import {
@@ -11,36 +10,7 @@ import {
   CasLogoutController,
 } from './controller';
 
-// Override the default CASAuthentication Logout function.
-/* eslint-disable @typescript-eslint/no-explicit-any */
-CASAuthentication.prototype.logout = function logoutOverride(req: any, res: Response): void {
-  // Destroy the entire session if the option is set.
-  if (this.destroy_session) {
-    req.session.destroy((err: any) => {
-      if (err) {
-        /* eslint-disable-next-line no-console */
-        console.log(err);
-      }
-    });
-  } else { // Otherwise, just destroy the CAS session variables.
-    delete req.session[this.session_name];
-    if (this.session_info) {
-      delete req.session[this.session_info];
-    }
-  }
-  // Redirect the client back to the page the request was sent from. **OVERRIDE**
-  res.redirect('back');
-};
-/* eslint-enable @typescript-eslint/no-explicit-any */
-
-/** Instantiate CASAuthentication */
-const cas = new CASAuthentication({
-  /* eslint-disable @typescript-eslint/camelcase */
-  cas_url: 'https://cas-auth.rpi.edu/cas',
-  service_url: process.env.NODE_ENV === 'production' ? 'https://rpicampusmap.herokuapp.com' : 'http://localhost:3000',
-  session_name: 'casUser',
-  /* eslint-enable @typescript-eslint/camelcase */
-});
+import cas from './controller'
 
 const jsonParser = bodyParser.json();
 const router = express.Router();
