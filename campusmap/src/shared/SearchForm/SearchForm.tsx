4f1c4cedfdfdd4a068d1c766115bc6ddcc599116
erikswanke <erikswanke01@gmail.com>
2021-03-26 18:15:36 -0400

    implemented search bar functionality

--- campusmap/src/shared/SearchForm/SearchForm.tsx
+++ campusmap/src/shared/SearchForm/SearchForm.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import { ReactElement } from 'react';
-import {
-  Form,
-  FormControl,
-  Button,
-} from 'react-bootstrap';
+
 
 const SearchForm = (): ReactElement => (
-  <Form inline>
-    <FormControl type="text" placeholder="Enter a location" className="mr-sm-2" />
-    <Button variant="light">Search</Button>
-  </Form>
+  <form action="/search" method="get">
+    <input type="text" placeholder="Enter a location" id="query" name="query" />
+    <button type="submit">Search</button>
+  </form>
 );
 
 export default SearchForm;
